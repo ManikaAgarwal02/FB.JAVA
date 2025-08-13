6641144b3550378c93a1d31b299bd125860ad45b
Manika Agarwal <manikaag29@gmail.com>
2025-08-13 09:12:17 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,8 @@
+public class p1 {
+    public static void main(String[] args) {
+        System.out.println("Manika Agarwal");
+        System.out.println("FB");
+        System.out.println("35");
+    }
+    
+}
