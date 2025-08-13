c5f07756f09e6dbee027a2dad4565b3b244101b5
Manika Agarwal <manikaag29@gmail.com>
2025-08-13 14:23:08 +0530

    Add files via upload

---
+++ p2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class p2 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter any number");
+        int c=sc.nextInt();
+        System.out.println("enter any number");
+        int b=sc.nextInt();
+        System.out.println(c%b);
+    }
+}
