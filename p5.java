24f9e046598152308115940529c0e5de7901807b
Manika Agarwal <manikaag29@gmail.com>
2025-08-14 09:55:00 +0530

    Add files via upload

---
+++ p5.java
@@ -0,0 +1,17 @@
+public class p5 {
+    public static void main(String[] args) {
+        int a=6,b=5;
+        System.out.println(a | b);
+        System.out.println(a & b);
+        System.out.println(a ^ b);
+        System.out.println(a << 2);
+        System.out.println(a >> 2);
+        System.out.println(~a);
+
+
+
+
+
+    }
+    
+}
