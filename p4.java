4713f0da3f5aa4a08b0a6b7c7485c9afe16ab2d7
Manika Agarwal <manikaag29@gmail.com>
2025-08-14 09:20:40 +0530

    Add files via upload

---
+++ p4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class p4 {
+    public static void main(String[] args) {
+       Scanner sc =new Scanner(System.in);
+       System.out.println("enter any number");
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       a=a+b;
+       b=a-b;
+       a=a-b;
+       System.out.println(a);
+       System.out.println(b);
+
+
+    }
+}
